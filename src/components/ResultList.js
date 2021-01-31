b63fe93458b5e17503c8dd4c51ca459d6a592cb8
James Y <35856061+jmyardley@users.noreply.github.com>
2021-01-31 16:05:34 -0500

    team sort

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 function ResultList(props) {
+  var arr = props.results;
+  var sorted = arr.sort(function(a,b) {
+    return b.winPercentage - a.winPercentage;
+  })
   return (
     <table className="table table-bordered table-sm">
       <tbody>
         <tr>
           <th>teamid</th><th>win</th><th>loss</th><th>gb</th><th>w%</th>
         </tr>
-        {props.results.map(result => (
+        {sorted.map(result => (
           <tr key={result.teamId}>
             <td>{result.teamId}</td><td>{result.win}</td><td>{result.loss}</td><td>{result.gamesBehind}</td><td>{result.winPercentage}</td>
           </tr>
